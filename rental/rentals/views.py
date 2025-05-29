6812ddc8c0eabd487bad421357bd78d3e50445f4
neoxxbtw <neoxx04110@gmail..com>
2025-05-29 16:01:53 +0300

    Фильтры

--- rental/rentals/views.py
+++ rental/rentals/views.py
@@ -45,6 +45,41 @@ def cars_list(request):
     cars = Car.objects.all()  # пример
     return render(request, 'rentals/cars_list.html', {'cars': cars})
 
+def cars_list(request):
+    cars = Car.objects.all()
+
+    # Фильтры
+    brand = request.GET.get('brand')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    min_year = request.GET.get('min_year')
+    max_year = request.GET.get('max_year')
+    max_mileage = request.GET.get('max_mileage')
+    sort_by = request.GET.get('sort_by')
+
+    if brand:
+        cars = cars.filter(brand__icontains=brand)
+    if min_price:
+        cars = cars.filter(rental_cost_per_day__gte=min_price)
+    if max_price:
+        cars = cars.filter(rental_cost_per_day__lte=max_price)
+    if min_year:
+        cars = cars.filter(year__gte=min_year)
+    if max_year:
+        cars = cars.filter(year__lte=max_year)
+    if max_mileage:
+        cars = cars.filter(mileage__lte=max_mileage)
+
+    if sort_by == 'price':
+        cars = cars.order_by('rental_cost_per_day')
+    elif sort_by == '-price':
+        cars = cars.order_by('-rental_cost_per_day')
+    elif sort_by == '-popularity':
+        cars = cars.order_by('-rental_count')
+
+    return render(request, 'rentals/cars_list.html', {
+        'cars': cars
+    })
 
 def rentals_list(request):
     rentals = Rental.objects.all()
@@ -52,3 +87,4 @@ def rentals_list(request):
 
 def rentals_list(request):
     return render(request, 'rentals/rentals_list.html')
+
