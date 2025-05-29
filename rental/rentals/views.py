6f90224b59e06ec1d14e30a596bbb1cd501f2639
neoxxbtw <neoxx04110@gmail..com>
2025-05-29 16:40:55 +0300

    Обновлены фильтры

--- rental/rentals/views.py
+++ rental/rentals/views.py
@@ -1,27 +1,53 @@
-from django.shortcuts import render
-from django.db.models import Count, Sum, Avg
-from django.utils.timezone import now
-from datetime import timedelta
-from .models import Car, Rental, Payment
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Count, Sum, Avg
+from .models import Car, Rental, Payment
 
 def landing_page(request):
     return render(request, 'rentals/index.html')
 
-
 def index(request):
+    # Поиск и фильтры
     search_query = request.GET.get('q', '')
+    brand = request.GET.get('brand')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    min_year = request.GET.get('min_year')
+    max_year = request.GET.get('max_year')
+    max_mileage = request.GET.get('max_mileage')
+    sort_by = request.GET.get('sort_by')
 
+    # Базовый queryset
     available_cars = Car.objects.filter(is_available=True)
+
     if search_query:
         available_cars = available_cars.filter(brand__icontains=search_query)
+    if brand:
+        available_cars = available_cars.filter(brand__icontains=brand)
+    if min_price:
+        available_cars = available_cars.filter(rental_cost_per_day__gte=min_price)
+    if max_price:
+        available_cars = available_cars.filter(rental_cost_per_day__lte=max_price)
+    if min_year:
+        available_cars = available_cars.filter(year__gte=min_year)
+    if max_year:
+        available_cars = available_cars.filter(year__lte=max_year)
+    if max_mileage:
+        available_cars = available_cars.filter(mileage__lte=max_mileage)
 
-    popular_cars = Car.objects.annotate(
-        rental_count=Count('rental')
-    ).order_by('-rental_count')[:3]
+    if sort_by == 'price':
+        available_cars = available_cars.order_by('rental_cost_per_day')
+    elif sort_by == '-price':
+        available_cars = available_cars.order_by('-rental_cost_per_day')
+    elif sort_by == '-popularity':
+        available_cars = available_cars.annotate(rental_count=Count('rental')).order_by('-rental_count')
 
-    latest_rentals = Rental.objects.order_by('-start_date')[:5]
+    # Популярные авто
+    popular_cars = Car.objects.annotate(rental_count=Count('rental')).order_by('-rental_count')[:3]
 
+    # Последние аренды
+    latest_rentals = Rental.objects.select_related('car', 'client').order_by('-start_date')[:5]
+
+    # Статистика платежей
     payments_stats = Payment.objects.aggregate(
         total_amount=Sum('amount'),
         count=Count('id'),
@@ -34,21 +60,17 @@ def index(request):
         'latest_rentals': latest_rentals,
         'payments_stats': payments_stats,
         'search_query': search_query,
+        'request': request,  # нужно для сохранения значений в шаблоне
     })
-
 
 def car_detail(request, pk):
     car = get_object_or_404(Car, pk=pk)
     return render(request, 'rentals/car_detail.html', {'car': car})
 
 def cars_list(request):
-    cars = Car.objects.all()  # пример
-    return render(request, 'rentals/cars_list.html', {'cars': cars})
-
-def cars_list(request):
     cars = Car.objects.all()
 
-    # Фильтры
+    # Аналогичная фильтрация для отдельной страницы
     brand = request.GET.get('brand')
     min_price = request.GET.get('min_price')
     max_price = request.GET.get('max_price')
@@ -75,16 +97,10 @@ def cars_list(request):
     elif sort_by == '-price':
         cars = cars.order_by('-rental_cost_per_day')
     elif sort_by == '-popularity':
-        cars = cars.order_by('-rental_count')
+        cars = cars.annotate(rental_count=Count('rental')).order_by('-rental_count')
 
-    return render(request, 'rentals/cars_list.html', {
-        'cars': cars
-    })
+    return render(request, 'rentals/cars_list.html', {'cars': cars})
 
 def rentals_list(request):
     rentals = Rental.objects.all()
     return render(request, 'rentals/rentals_list.html', {'rentals': rentals})
-
-def rentals_list(request):
-    return render(request, 'rentals/rentals_list.html')
-
